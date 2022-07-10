e4fc77888c8d6f40f02f7b77cf30215b857a24da
Skendoul Mohamed <mohamed.skendoul@30mins.com>
2022-07-11 00:02:16 +0200

    DFDFDF

--- components/tour/tour-components/tour-details/OverviewHighlitesPlaces.tsx
+++ components/tour/tour-components/tour-details/OverviewHighlitesPlaces.tsx
@@ -1,28 +1,22 @@
 import React from "react";
 import Style from "../../../../styles/pages/tour/tour-components/tour-details/overview-highlites-places.module.css";
-// const DestinationsSamllCard
-
-
+import DestinationsSamllCard from "../../../destinations/destinations-components/DestinationsSamllCard";
 
 const OverviewHighlitesPlaces = () => {
-  const Bg =
-    "https://morokotour.vercel.app/images/ait-ben-haddou-images/ourzazate-aitbenhadou3.jpg";
+  const Overview =
+    "Discover the Berber villages of the Atlas Mountains on a full-day trip from Marrakech that explores broad valleys and lush orchards with views of Mount Toubkal, the highest peak in North Africa. Ride a camel through a broad gorge, pause for tea in Imlil Valley, then hike to the isolated village of Ait Souka to share a meal in a Berber family’s home. Throughout the day, take in the mountains’ beautiful scenery: terraced gardens that climb steep hillsides, tumbling waterfalls, and an alpine kasbah.";
   return (
     <div className={Style.container}>
       <b className={Style.detail_title}>Places You’ll See</b>
       <div className={Style.places}>
-        <div className={Style.container_place}>
-          <div
-            className={Style.container_img}
-            style={{ backgroundImage: `url(${Bg})` }}
-          ></div>
-          <div className={Style.container_desc}>
-            <b className={Style.title}>Marrakech</b>
-            <p className={Style.desc}>More then 1222 Tours </p>
-          </div>
-        </div>
+        <DestinationsSamllCard />
+        <DestinationsSamllCard />
+        <DestinationsSamllCard />
+        <DestinationsSamllCard />
+        <DestinationsSamllCard />
       </div>
       <b className={Style.detail_title}>Overview</b>
+      {Overview}
     </div>
   );
 };
