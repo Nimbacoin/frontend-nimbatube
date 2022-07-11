82520e161ef5ab70023d67b17da2cac87bcc40ac
Skendoul Mohamed <mohamed.skendoul@30mins.com>
2022-07-11 23:15:58 +0200

    SD

--- redux/store.ts
+++ redux/store.ts
@@ -2,11 +2,9 @@ import SideMenu from "./style-slice/menu/SideMenu";
 import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import { createWrapper } from "next-redux-wrapper";
-import messageSliceReducer from "./message-slice/MessageSlice";
 
 export const store = configureStore({
   reducer: {
-    messageSliceReducer,
     SideMenu,
   },
   middleware: [thunk],
