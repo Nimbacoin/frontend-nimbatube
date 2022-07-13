33b9e32edcc336ba2a182c5df39d70a0f9428185
Skendoul Mohamed <mohamed.skendoul@30mins.com>
2022-07-13 14:49:30 +0200

    DSDSD

--- redux/store.ts
+++ redux/store.ts
@@ -2,7 +2,7 @@ import SideMenu from "./style-slice/menu/SideMenu";
 import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import { createWrapper } from "next-redux-wrapper";
-
+//FGFG
 export const store = configureStore({
   reducer: {
     SideMenu,
