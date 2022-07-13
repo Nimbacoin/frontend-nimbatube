cc8d72a4a430ef2c3e92258f824fe6d984f49a02
Skendoul Mohamed <mohamed.skendoul@30mins.com>
2022-07-13 14:33:41 +0200

    SDSDdda

--- layout/header/HeaderRight.tsx
+++ layout/header/HeaderRight.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 import Style from "../../styles/layout/header/header-compnents/header-right.module.css";
-import MyAccount from "./header-components/MyAccount";
-import Bookings from "./header-components/Bookings";
-import WishList from "./header-components/WishList";
-import LangAndCurrency from "./header-components/LangAndCurrency";
 const HeaderRight = () => {
   return (
     <div className={Style.container}>
