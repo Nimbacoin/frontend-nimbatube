d5c0968ab263d615f02e4beb70dac2efe9318f87
Skendoul Mohamed <mohamed.skendoul@30mins.com>
2022-07-13 19:15:29 +0200

    SDSDSDSDSD

--- layout/header/header-components/HeaderDropDown.tsx
+++ layout/header/header-components/HeaderDropDown.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState, useRef } from "react";
 import Style from "../../../styles/layout/header/header-compnents/header-drop-down.module.css";
 import { IoCloudUploadOutline } from "@react-icons/all-files/io5/IoCloudUploadOutline";
+import { IoPersonOutline } from "@react-icons/all-files/io5/IoPersonOutline";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { link } from "fs";
 
 const HeaderDropDown = () => {
   const [ShowDiv, setShowDiv] = useState(false);
-
+  const Router = useRouter();
   const Ref = React.useRef<HTMLDivElement>(null);
   const InputSearch = React.useRef<HTMLDivElement>(null);
 
@@ -31,25 +34,48 @@ const HeaderDropDown = () => {
       link: "uploads",
       icon: <IoCloudUploadOutline />,
       classname: Style.link_container,
+      username : "@mrbeast",
+      chanelname: "MrBeasr",
+    },
+    {
+      name: "Uploads",
+      link: "uploads",
+      icon: <IoCloudUploadOutline />,
+      classname: Style.link_container,
+    },
+    {
+      name: "Sing Out",
+      link: "Sing Out",
+      icon: <IoCloudUploadOutline />,
+      classname: Style.link_container,
     },
   ];
-
+  const HandelClick = (e: any, link: string) => {
+    if (e.target.id === "Sing Out") {
+      alert(e);
+    } else {
+      Router.push(link);
+    }
+  };
   return (
     <>
       {(() => {
         return (
           <div className={Style.container}>
             <div className={Style.drop_down_option} ref={InputSearch}>
-              Drop
+              <IoPersonOutline />
             </div>
             {ShowDiv && (
               <div className={Style.drop_down_container} ref={Ref}>
-                {AllLink.map(({ name, link, icon, classname }) => (
-                  <Link href={link}>
-                    <div className={classname}>
-                      {name} {icon}
-                    </div>
-                  </Link>
+                {AllLink.map(({ link, name, icon, classname }) => (
+                  <div
+                    key={name}
+                    onClick={(e) => HandelClick(e, link)}
+                    className={classname}
+                    id={name}
+                  >
+                    {name} {icon}
+                  </div>
                 ))}
               </div>
             )}
