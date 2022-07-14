0c7d13886d7aa53fade8fd2d05d09a75f96d02d0
Skendoul Mohamed <mohamed.skendoul@30mins.com>
2022-07-14 11:15:29 +0200

    SDSDDSD

--- components/watch/watch-page/left-side/MainVideo.tsx
+++ components/watch/watch-page/left-side/MainVideo.tsx
@@ -1,16 +1,14 @@
 import React, { useEffect, useRef } from "react";
 import ReactPlayer from "react-player";
+import Style from "../../../../styles/pages/watch/leftside/leftside.module.css";
 
 const MainVideo = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  
   return (
-    <ReactPlayer
-      playing={true}
-      url="https://www.w3schools.com/tags/movie.mp4"
-      controls
-    />
+    <div className={Style.video_container}>
+      <ReactPlayer playing={true} url="https://youtu.be/yI12AC94ado" controls />
+    </div>
   );
 };
 
