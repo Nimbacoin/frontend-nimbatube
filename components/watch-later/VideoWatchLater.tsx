b42fb1ac825ab4917bdb6bdd562d4687eafb69d1
Skendoul Mohamed <mohamed.skendoul@30mins.com>
2022-07-19 20:37:38 +0200

    SDSDSD

--- components/watch-later/VideoWatchLater.tsx
+++ components/watch-later/VideoWatchLater.tsx
@@ -7,17 +7,26 @@ const VideoWatchLater = () => {
     "https://scontent.fvit1-1.fna.fbcdn.net/v/t39.30808-6/288368479_5078956948868473_6446883941384535955_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=843cd7&_nc_ohc=QVdNmwF0fz0AX9TaBdI&_nc_ht=scontent.fvit1-1.fna&oh=00_AT8WMfSBJToD6yYf9L12f5oau_uE1Cjj6R15cLiUa_GukQ&oe=62D3771B";
   return (
     <div className={Style.container}>
-      <div
-        style={{ backgroundImage: `url(${Bg})` }}
-        className={Style.img}
-      ></div>
-      <div className={Style.chanel_container}>
+      <div className={Style.video_container}>
+        <video width="100%" height="30px" autoPlay muted loop>
+          <source
+            src="https://www.w3schools.com/html/movie.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </div>
+      <div className={Style.video_date_containe}>
         <span className={Style.chanel_name}>MrBeast</span>
-        <span className={Style.username}>@MrBeast</span>
-        <p className={Style.chanel_followers}>
-          <span className={Style.Followers}>100 Followers - 100 Uploads</span>
-        </p>
-        <button className={Style.follow_button}>Follow</button>
+        <div className={Style.chanel_container}>
+          <div
+            style={{ backgroundImage: `url(${Bg})` }}
+            className={Style.img}
+          ></div>
+          <p className={Style.chanel_followers}>
+            <span className={Style.username}>MrBeast</span>
+            <span className={Style.Followers}>20 Days Ago</span>
+          </p>
+        </div>
       </div>
       <div className={Style.right_container}>
         <button className={Style.IoEllipsisVertical}>
--- components/watch-later/WatchLater.tsx
+++ components/watch-later/WatchLater.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import VideoWatchLater from "./VideoWatchLater";
+import Style from "../../styles/pages/watch-later/watch-later.module.css";
+
 const WatchLater = () => {
   return (
-    <div>
+    <div className={Style.container}>
+      <VideoWatchLater />
+
+      <VideoWatchLater />
+      <VideoWatchLater />
+      <VideoWatchLater />
+      <VideoWatchLater />
       <VideoWatchLater />
     </div>
   );
