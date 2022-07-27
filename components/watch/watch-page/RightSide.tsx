4f0d31e7c83d91e08c0dd05987460800aacb7e84
Skendoul Mohamed <mohamed.skendoul@30mins.com>
2022-07-27 21:19:47 +0200

    SDSDSD

--- components/watch/watch-page/RightSide.tsx
+++ components/watch/watch-page/RightSide.tsx
@@ -1,28 +1,23 @@
-import React from "react";
+import { trace } from "console";
+import React, { useEffect, useState } from "react";
 import Style from "../../../styles/pages/watch/rightside/rightside.module.css";
 import Vedio from "../../vedio/Vedio";
 import VideoOption from "./right-side/VideoOption";
 
 const RightSide = () => {
+  const [IsPhone, setIsPhone] = useState(false);
+  useEffect(() => {
+    window.onresize = () => {
+      if (window.innerWidth <= 1149) {
+        setIsPhone(true);
+      } else if (window.innerWidth >= 1149) {
+        setIsPhone(false);
+      }
+    };
+  });
   return (
     <div className={Style.container}>
-      <VideoOption />
-      <VideoOption />
-      <VideoOption />
-      <VideoOption />
-      <VideoOption />
-      <VideoOption />
-      <VideoOption />
-      <VideoOption />
-      <VideoOption />
-      <VideoOption />
-      <VideoOption />
-      <VideoOption />
-      <VideoOption />
-      <VideoOption />
-      <VideoOption />
-      <VideoOption />
-      <Vedio />
+      {IsPhone ? <Vedio /> : <VideoOption />}
     </div>
   );
 };
