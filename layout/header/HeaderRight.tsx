4cbc6b5f0c837446f806361eea50b96f9db7ded7
Skendoul Mohamed <mohamed.skendoul@30mins.com>
2022-08-06 00:55:41 +0200

    DFDFDF

--- layout/header/HeaderRight.tsx
+++ layout/header/HeaderRight.tsx
@@ -1,26 +1,30 @@
 import React from "react";
 import Style from "../../styles/layout/header/header-compnents/header-right.module.css";
 import { useRouter } from "next/router";
+import HeaderRightPhoneUserLogedOut from "./header-components/HeaderRightPhoneUserLogedOut";
 const HeaderRight = () => {
   const Router = useRouter();
   const HandelPush = (Path: string) => {
     Router.push(Path);
   };
   return (
-    <div className={Style.container}>
-      <button
-        onClick={() => HandelPush("/auth/sign-in")}
-        className={Style.button_log_in}
-      >
-        Sigin in
-      </button>
-      <button
-        onClick={() => HandelPush("/auth/sign-up")}
-        className={Style.button_singup}
-      >
-        Sign up
-      </button>
-    </div>
+    <>
+      <HeaderRightPhoneUserLogedOut />
+      <div className={Style.container}>
+        <button
+          onClick={() => HandelPush("/auth/sign-in")}
+          className={Style.button_log_in}
+        >
+          Sigin in
+        </button>
+        <button
+          onClick={() => HandelPush("/auth/sign-up")}
+          className={Style.button_singup}
+        >
+          Sign up
+        </button>
+      </div>
+    </>
   );
 };
 
---
+++ layout/header/header-components/HeaderRightPhoneUserLogedOut.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState, useRef } from "react";
+import Style from "../../../styles/layout/header/header-compnents/header-right-phone-user-loged-out.module.css";
+import { IoCloudUploadOutline } from "@react-icons/all-files/io5/IoCloudUploadOutline";
+import { IoPersonOutline } from "@react-icons/all-files/io5/IoPersonOutline";
+import { IoPeopleOutline } from "@react-icons/all-files/io5/IoPeopleOutline";
+import { IoSettingsOutline } from "@react-icons/all-files/io5/IoSettingsOutline";
+import { IoMdHelpCircleOutline } from "@react-icons/all-files/io/IoMdHelpCircleOutline";
+import { useRouter } from "next/router";
+
+
+const HeaderRightPhoneUserLogedOut = () => {
+  const [ShowDiv, setShowDiv] = useState(false);
+  const Router = useRouter();
+  const Ref = React.useRef<HTMLDivElement>(null);
+  const InputSearch = React.useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const HandelClick = (e: any) => {
+      if (InputSearch && InputSearch.current) {
+        const refany = InputSearch.current;
+        if (refany.contains(e.target)) {
+          setShowDiv(!ShowDiv);
+        } else if (Ref && Ref.current) {
+          const refany = Ref.current;
+          if (!refany.contains(e.target)) {
+            setShowDiv(false);
+          }
+        }
+      }
+    };
+    window.addEventListener("click", HandelClick);
+  }, [ShowDiv]);
+  const AllLink = [
+    {
+      name: "Sign in",
+      link: "/auth/sign-in",
+      icon: <IoCloudUploadOutline />,
+    },
+    {
+      name: "Sign up",
+      link: "/auth/sign-up",
+      icon: <IoPeopleOutline />,
+    },
+    {
+      name: "Settings",
+      link: "/settings",
+      icon: <IoSettingsOutline />,
+    },
+
+    {
+      name: "Help",
+      link: "/help",
+      icon: <IoMdHelpCircleOutline />,
+    },
+  ];
+  const HandelClick = (e: any, link: string) => {
+    Router.push(link);
+  };
+  return (
+    <>
+      {(() => {
+        return (
+          <div className={Style.container}>
+            <div className={Style.drop_down_option} ref={InputSearch}>
+              <IoPersonOutline />
+            </div>
+            {ShowDiv && (
+              <div className={Style.drop_down_container} ref={Ref}>
+                {AllLink.map(({ link, name, icon }) => (
+                  <div
+                    key={name}
+                    onClick={(e: any) => HandelClick(e, link)}
+                    className={Style.link_container}
+                    id={link}
+                  >
+                    {icon && <span className={Style.icon}> {icon}</span>}
+                    <div className={Style.link_data}>
+                      {name && <span className={Style.name}> {name}</span>}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        );
+      })()}
+    </>
+  );
+};
+
+export default HeaderRightPhoneUserLogedOut;
