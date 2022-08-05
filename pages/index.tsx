97b5a67cd07513a3bc52b1ef81c1bbb3497bc4f9
Skendoul Mohamed <mohamed.skendoul@30mins.com>
2022-08-05 20:47:59 +0200

    SDSDSDSDDAAA!

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,21 +5,7 @@ import Home from "../components/home/Home";
 import BasedUrlRequest from "../utils/basedGetUrlRequest";
 
 const HomePage = () => {
-  const { data } = useSession();
-  // useEffect(() => {
-  //   const Fetching = () => {
-  //     BasedUrlRequest("/api/hello").then((HSD) => {
-  //       console.log(HSD);
-  //     });
-  //   };
-  //   Fetching();
-  // }, []);
-  BasedUrlRequest("/api/hello").then((HSD) => {
-    console.log(HSD);
-  });
-  let UserId = data?.user?.email;
-  let Tokken = data?.accessToken;
-  console.log(data, Tokken, UserId);
+ 
   return <Home />;
 };
 
