002125d36e0e080063c9face0787dcc49b63e8a0
Skendoul Mohamed <mohamed.skendoul@30mins.com>
2022-08-08 19:49:17 +0200

    SDSDSD

--- components/chanel/my-chanel/MyChanelPage.tsx
+++ components/chanel/my-chanel/MyChanelPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProfileDate from "./newpage-component/ProfileDate";
-import Style from "../../../styles/pages/chanel/my-chanel/my-chanel.module.css";
+// import Style from "../../../styles/pages/chanel/my-chanel/my-chanel.module.css";
+import Style from "../../../styles/pages/chanel/my-chanel/my-chanel.module.css"
 const MychanelPage = () => {
   return (
     <div className={Style.container}>
