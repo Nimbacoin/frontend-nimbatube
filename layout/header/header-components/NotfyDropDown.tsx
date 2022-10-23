6e61c990e3173daa5ae0d0de3f9a6c46748a6b92
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-10-23 21:22:38 +0200

    SF

--- layout/header/header-components/NotfyDropDown.tsx
+++ layout/header/header-components/NotfyDropDown.tsx
@@ -17,50 +17,52 @@ const NotfyDropDown = ({ Notification }: any) => {
     Router.push("/watch/watch?watching=true&video=" + videoData?._id);
   };
   return (
-    <div className={Style.drop_down_container}>
-      {notification.length
-        ? notification.map(({ channelData, videoData, vid }: any) => (
-            <div
-              onClick={() => {
-                hadnelClick(videoData);
-              }}
-              className={Style.main_container}
-            >
-              <div className={Style.img_container}>
-                <div
-                  style={{
-                    backgroundImage: `url(${
-                      process.env.NEXT_PUBLIC_BACK_END_URL +
-                      "/api/get/read/images/" +
-                      channelData?.channelData?.profileImg?.url
-                    })`,
-                  }}
-                  className={Style.channel_img}
-                ></div>
+    <div className={Style.container}>
+      <div className={Style.drop_down_container}>
+        {notification.length
+          ? notification.map(({ channelData, videoData, vid }: any) => (
+              <div
+                onClick={() => {
+                  hadnelClick(videoData);
+                }}
+                className={Style.main_container}
+              >
+                <div className={Style.img_container}>
+                  <div
+                    style={{
+                      backgroundImage: `url(${
+                        process.env.NEXT_PUBLIC_BACK_END_URL +
+                        "/api/get/read/images/" +
+                        channelData?.channelData?.profileImg?.url
+                      })`,
+                    }}
+                    className={Style.channel_img}
+                  ></div>
+                </div>
+                <div className={Style.desc_container}>
+                  <p className={Style.text}>
+                    {channelData?.channelData?.name + videoData?.title}
+                  </p>
+                  <p className={Style.text_date}>
+                    {moment(vid?.from?.createAt).startOf("hour").fromNow()}
+                  </p>
+                </div>
+                <div className={Style.video_container}>
+                  <div
+                    style={{
+                      backgroundImage: `url(${
+                        process.env.NEXT_PUBLIC_BACK_END_URL +
+                        "/api/get/read/images/" +
+                        videoData?.thumbnail
+                      })`,
+                    }}
+                    className={Style.video_img}
+                  ></div>
+                </div>
               </div>
-              <div className={Style.desc_container}>
-                <p className={Style.text}>
-                  {channelData?.channelData?.name + videoData?.title}
-                </p>
-                <p className={Style.text_date}>
-                  {moment(vid?.from?.createAt).startOf("hour").fromNow()}
-                </p>
-              </div>
-              <div className={Style.video_container}>
-                <div
-                  style={{
-                    backgroundImage: `url(${
-                      process.env.NEXT_PUBLIC_BACK_END_URL +
-                      "/api/get/read/images/" +
-                      videoData?.thumbnail
-                    })`,
-                  }}
-                  className={Style.video_img}
-                ></div>
-              </div>
-            </div>
-          ))
-        : ""}{" "}
+            ))
+          : ""}{" "}
+      </div>
     </div>
   );
 };
