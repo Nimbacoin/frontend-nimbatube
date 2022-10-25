96d01877b34054abcba68ae4aa2fb3bb334c8f6d
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-10-25 14:21:07 +0200

    DF

--- components/watch/watch-page/right-side/RightSideTaggs.tsx
+++ components/watch/watch-page/right-side/RightSideTaggs.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Style from "../../../../styles/pages/watch/rightside/right-side-taggs.module.css";
 import { IoChevronForwardOutline } from "@react-icons/all-files/io5/IoChevronForwardOutline";
+import { IoChevronBackOutline } from "@react-icons/all-files/io5/IoChevronBackOutline";
 const RightSideTaggs = () => {
   // const [videos, setVideos] = useState([]);
   const [activVideos, setActivVideos] = useState(0);
@@ -33,11 +34,41 @@ const RightSideTaggs = () => {
       const scrool = divContainer.current.scrollLeft + 50;
       if (scrool <= divContainer.current.scrollWidth) {
         divContainer.current.scrollLeft = scrool;
+        setScrollValue(divContainer.current.scrollLeft);
       }
     }
   };
+  const handelClickLeft = () => {
+    if (divContainer.current) {
+      if (divContainer.current.scrollLeft >= 1) {
+        divContainer.current.scrollLeft = divContainer.current.scrollLeft - 50;
+        setScrollValue(divContainer.current.scrollLeft);
+      }
+    }
+  };
+  const [scrollZero, setScrollZero] = useState(false);
+  const [scrollValue, setScrollValue] = useState(0);
+
+  useEffect(() => {
+    if (divContainer.current) {
+      if (divContainer.current.scrollLeft >= 1) {
+        setScrollZero(true);
+      } else {
+        setScrollZero(false);
+      }
+    }
+  }, [scrollValue]);
+
   return (
     <div className={Style.container}>
+      {scrollZero && (
+        <div className={Style.div_scroll_left}>
+          <span onClick={handelClickLeft} className={Style.forward_outline}>
+            <IoChevronBackOutline />
+          </span>
+        </div>
+      )}
+
       <div ref={divContainer} className={Style.main_container}>
         {Tags.map(({ name }, index) => (
           <span
