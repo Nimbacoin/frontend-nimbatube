dfe8862aaa2c4d5b30b522bbb5891e1f8cbfc42d
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-11-05 22:24:50 +0100

    DF

--- components/channel/my-channel/newpage-component/ActiveVideo.tsx
+++ components/channel/my-channel/newpage-component/ActiveVideo.tsx
@@ -4,10 +4,24 @@ import Style from "../../../../styles/pages/channel/my-channel/my-channel-compon
 import TextArea from "../../../modals/TextArea";
 import BlueButton from "../../../modals/BlueButton";
 import OtherChannelData from "./OtherChannelData";
+import SmallTextBlack from "../../../modals/SmallTextBlack";
 const ActiveVideo = ({ Name, Id }: any) => {
-  return <div className={Style.container}>
-    <div className={Style.container_video}></div>
-  </div>;
+  return (
+    <div className={Style.container}>
+      <div className={Style.container_video}>
+        <video className={Style.video_tag} muted autoPlay controls loop>
+          <source
+            src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+            className={Style.video}
+            type="video/mp4"
+          />
+        </video>
+      </div>
+      <div className={Style.container_video_data}>
+        <SmallTextBlack Text="Shawn Mendes, Tainy - Summer Of Love (Official Music Video)" />
+      </div>
+    </div>
+  );
 };
 
 export default ActiveVideo;
