1c8dd39802e84174af99b44e6c62f63dddfbcf52
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-11-11 16:56:26 +0100

    SF

--- components/modals/ButtonLive.tsx
+++ components/modals/ButtonLive.tsx
@@ -1,27 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Style from "../../styles/modals/button-live.module.css";
 import VideoCallSharpIcon from "@mui/icons-material/VideoCallSharp";
 import { FcVideoCall } from "@react-icons/all-files/fc/FcVideoCall";
 import SmallTextBlack from "./SmallTextBlack";
+import Link from "next/link";
 const ButtonLive = () => {
-  const HandelClick = () => {};
-
+  const HandelClick = () => {
+    setShowDiv(!showDiv);
+  };
+  const [showDiv, setShowDiv] = useState(false);
   return (
-    <div onClick={HandelClick} className={Style.wrap}>
-      <div className={Style.div_conntnt}>
-        <div className={Style.container_icons}>
-          <div className={Style.container_icon}>
-            <div className={Style.icon}></div>
-            <SmallTextBlack Text={"upload"} />
+    <div className={Style.wrap}>
+      {showDiv && (
+        <div className={Style.div_conntnt}>
+          <div className={Style.container_icons}>
+            <Link href="/go-live//go-live">
+              <div className={Style.container_icon}>
+                <div className={Style.icon}></div>
+                <SmallTextBlack Text={"upload"} />
+              </div>
+            </Link>
+            <Link href="/go-live">
+              <div className={Style.container_icon_2}>
+                <div className={Style.icon}></div>
+                <SmallTextBlack Text={"go live"} />
+              </div>
+            </Link>
           </div>
-          <div className={Style.container_icon_2}>
-            <div className={Style.icon}></div>
-            <SmallTextBlack Text={"go live"} />
-          </div>
+
+          <div className={Style.triangle_right}></div>
         </div>
-        <div className={Style.triangle_right}></div>
-      </div>
-      <button className={Style.button}>
+      )}
+      <button onClick={HandelClick} className={Style.button}>
         <VideoCallSharpIcon />
       </button>
     </div>
