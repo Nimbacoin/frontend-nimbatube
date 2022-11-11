c478c6f9000f9a909ad356896c8ab7123c7b43cd
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-11-11 15:33:08 +0100

    SF

--- components/modals/ButtonLive.tsx
+++ components/modals/ButtonLive.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import Style from "../../styles/modals/button-live.module.css";
 import VideoCallSharpIcon from "@mui/icons-material/VideoCallSharp";
-import {FcVideoCall} from "@react-icons/all-files/fc/FcVideoCall";
+import { FcVideoCall } from "@react-icons/all-files/fc/FcVideoCall";
 const ButtonLive = () => {
   const HandelClick = () => {};
+
   return (
     <div onClick={HandelClick} className={Style.wrap}>
       <div className={Style.div_conntnt}>
         <div className={Style.container_icons}>
-             <FcVideoCall />
+          <div className={Style.container_icon}>
+            <div className={Style.icons}></div>
+          </div>
+          <div className={Style.container_icon_2}>
+            <div className={Style.icon}></div>
+          </div>
         </div>
-        <div className={Style.triangle_right}>
-        
-         
-        </div>
+        <div className={Style.triangle_right}></div>
       </div>
       <button className={Style.button}>
         <VideoCallSharpIcon />
