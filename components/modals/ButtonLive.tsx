372be8c3a4f486aab93d6e6e834aff5bc2656d68
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-11-11 16:15:18 +0100

    DF

--- components/modals/ButtonLive.tsx
+++ components/modals/ButtonLive.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Style from "../../styles/modals/button-live.module.css";
 import VideoCallSharpIcon from "@mui/icons-material/VideoCallSharp";
 import { FcVideoCall } from "@react-icons/all-files/fc/FcVideoCall";
+import SmallTextBlack from "./SmallTextBlack";
 const ButtonLive = () => {
   const HandelClick = () => {};
 
@@ -10,10 +11,12 @@ const ButtonLive = () => {
       <div className={Style.div_conntnt}>
         <div className={Style.container_icons}>
           <div className={Style.container_icon}>
-            <div className={Style.icons}></div>
+            <div className={Style.icon}></div>
+            <SmallTextBlack Text={"upload"} />
           </div>
           <div className={Style.container_icon_2}>
             <div className={Style.icon}></div>
+            <SmallTextBlack Text={"go live"} />
           </div>
         </div>
         <div className={Style.triangle_right}></div>
