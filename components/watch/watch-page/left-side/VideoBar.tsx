cacd166a41c3073b75495de0f8ba1ccfebfefad5
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-11-12 02:35:26 +0100

    DF

--- components/watch/watch-page/left-side/VideoBar.tsx
+++ components/watch/watch-page/left-side/VideoBar.tsx
@@ -3,7 +3,7 @@ import Style from "../../../../styles/pages/watch/leftside/video-bar.module.css"
 
 const VideoBar = ({ Width, HandelClick }: any) => {
   const [isMouseDown, setIsMouseDown] = useState(false);
-   const [changeClassName, setChangeClassName] = useState(false);
+  const [changeClassName, setChangeClassName] = useState(false);
   const progressSlider = React.useRef<HTMLDivElement | null>(null);
   const progressFill = React.useRef<HTMLDivElement | null>(null);
 
@@ -25,24 +25,39 @@ const VideoBar = ({ Width, HandelClick }: any) => {
   const onMouseUpActive = () => {
     setIsMouseDown(false);
   };
-  
+
+  const functionToChangeTime = () => {
+    if (progressSlider.current && progressFill.current) {
+      const newTime =
+        e.nativeEvent.offsetX / progressSlider.current.offsetWidth;
+      console.log(e.nativeEvent.offsetX);
+      progressFill.current.style.width = `${newTime * 100}%`;
+      HandelClick(newTime);
+    }
+  };
   const onMouseOverActive = (e: any) => {
     if (isMouseDown) {
-      if (progressSlider.current && progressFill.current) {
-        const newTime =
-          e.nativeEvent.offsetX / progressSlider.current.offsetWidth;
-        console.log(e.nativeEvent.offsetX);
-        progressFill.current.style.width = `${newTime * 100}%`;
-        HandelClick(newTime);
-      }
+      functionToChangeTime();
     }
   };
   useEffect(() => {}, []);
-
+  const handelTouchStart = (e: any) => {
+    console.log("start");
+  };
+  const handelTouchMove = (e: any) => {
+    console.log("move");
+    functionToChangeTime();
+  };
+  const handelonTouchEnd = (e: any) => {
+    console.log("end");
+  };
   return (
     <div
       ref={progressSlider}
       onClick={handelChange}
+      onTouchStart={handelTouchStart}
+      onTouchMove={handelTouchMove}
+      onTouchEnd={handelonTouchEnd}
       onMouseLeave={onMouseLeaveActive}
       onMouseDown={onMouseDownActive}
       onMouseUp={onMouseUpActive}
