4f229d6ee2382a3c144d3521fa45ad87b530006b
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-11-16 00:31:14 +0100

    FD

--- components/watch/WatchPage.tsx
+++ components/watch/WatchPage.tsx
@@ -1,41 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import LeftSide from "./watch-page/LeftSide";
 import RightSide from "./watch-page/RightSide";
 import Style from "../../styles/pages/watch/watch.module.css";
 import { useDispatch } from "react-redux";
 import { MainVideoDataReducer } from "../../redux/video-slice/VideoSlice";
-import basedGetUrlRequest from "../../utils/basedGetUrlRequest";
-import { useRouter } from "next/router";
-const WatchPage = () => {
-  const [videoData, setVideoData] = useState({});
+const WatchPage = ({ VideoData }: any) => {
+  //VideoData
+  // console.log("watch page", VideoData);
   const dispatch = useDispatch();
-  const { asPath } = useRouter();
-
-  useEffect(() => {
-    const localFetch = async () => {
-      let Params = new URL(window.location.href).searchParams;
-      const video: string | null = Params.get("video");
-      await basedGetUrlRequest(
-        "/api/get/video/" +
-          video +
-          "/" +
-          "1232223923020290230923" +
-          "/" +
-          "cookies",
-        true
-      ).then((res) => {
-        console.log(res);
-        setVideoData(res);
-        dispatch(MainVideoDataReducer({ message: "data", data: res }));
-      });
-    };
-    localFetch();
-  }, [asPath]);
-
+  dispatch(MainVideoDataReducer({ message: "data", data: VideoData }));
   return (
     <div className={Style.container}>
       <LeftSide />
-      <RightSide videoData={videoData} />
+      <RightSide />
     </div>
   );
 };
