774974e0054a351b0b8f648b05fd7d2fd32605d6
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-11-28 19:27:13 +0100

    DG

--- components/wallet/WalletPage.tsx
+++ components/wallet/WalletPage.tsx
@@ -3,6 +3,7 @@ import TopTitle from "../modals/TopTitle";
 import Style from "../../styles/pages/wallet/wallet.module.css";
 import { IoWalletOutline } from "@react-icons/all-files/io5/IoWalletOutline";
 import WalletsConntent from "./wallet-comp/WalletsConntent";
+import { isAndroid, isIOS } from "react-device-detect";
 
 const WalletPage = () => {
   return (
@@ -16,6 +17,17 @@ const WalletPage = () => {
       <div className={Style.container_wallets_option}>
         <WalletsConntent />
       </div>
+      {isAndroid ? (
+        <a href="https://play.google.com/store/apps/details?id=com.instagram.android">
+          Open Android app
+        </a>
+      ) : isIOS ? (
+        <a href="https://apps.apple.com/us/app/instagram/id389801252">
+          Open iOS app
+        </a>
+      ) : (
+        <a href="https://instagram.com">Open Web app</a>
+      )}
     </div>
   );
 };
