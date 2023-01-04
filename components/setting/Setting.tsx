5ab63022edcbfa4c1d17696877a34cf37a22b90f
mohadeev <94206815+mohadeev@users.noreply.github.com>
2023-01-04 06:07:47 +0100

    DF

---
+++ components/setting/Setting.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Style from "../../styles/pages/setting/setting.module.css";
+const Settings = () => {
+  return (
+    <div className={Style.container}>
+      <div className={Style.side_bar}>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
