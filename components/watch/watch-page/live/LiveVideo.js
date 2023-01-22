4073aca13376175608f8946e008bccebe76cf105
mohadeev <94206815+mohadeev@users.noreply.github.com>
2023-01-22 12:23:24 +0100

    DF

--- components/watch/watch-page/live/LiveVideo.js
+++ components/watch/watch-page/live/LiveVideo.js
@@ -30,54 +30,6 @@ const App = () => {
   const [videoId, setVideoId] = useState("");
 
   useEffect(() => {
-    let Params = new URL(window.location.href).searchParams;
-    const video = Params.get("video");
-    const watching = Params.get("streaming");
-    if (video?.length && video?.length > 10) {
-      setVideoId(video);
-    }
-  }, [asPath]);
-
-  let peerConnection;
-
-  useEffect(() => {
-    socket.on("offer", (id, description) => {
-      peerConnection = new RTCPeerConnection(pc_config);
-      peerConnection
-        .setRemoteDescription(description)
-        .then(() => peerConnection.createAnswer())
-        .then((sdp) => peerConnection.setLocalDescription(sdp))
-        .then(() => {
-          socket.emit("answer", id, peerConnection.localDescription);
-        });
-      peerConnection.ontrack = (event) => {
-        videoRef.current.srcObject = event.streams[0];
-        dispatch(videoLiveIsConnectedReducer());
-      };
-      peerConnection.onicecandidate = (event) => {
-        if (event.candidate) {
-          socket.emit("candidate", id, event.candidate);
-        }
-      };
-    });
-  }, [socket]);
-
-  useEffect(() => {
-    socket.on("candidate", (id, candidate) => {
-      peerConnection
-        .addIceCandidate(new RTCIceCandidate(candidate))
-        .catch((e) => console.error(e));
-    });
-  }, [socket]);
-  useEffect(() => {
-    socket.on("connect_error", (err) => {
-      console.log(`connect_error due to the ${err.message}`);
-    });
-    socket.on("new-broadcaster", (id) => {
-      setBroadcasterId(id);
-    });
-  });
-  useEffect(() => {
     socket.on("new-live-record", (data) => {
       const blob = new Blob(data, { type: "video/mp4" });
       // second.current.src = blob;
@@ -85,16 +37,6 @@ const App = () => {
       videoRef.current.src = url;
     });
   }, [socket]);
-
-  useEffect(() => {
-    if (videoId.length > 10) {
-      socket.emit("watcher", { broadcasterId, videoId });
-    }
-    if (document.readyState == "complete") {
-      // alert(document.readyState);
-      //
-    }
-  }, [videoId]);
 
   return (
     <>
